6058e1111c52c8bec33452b5c0b4ad91c29a369b
RittickDatta <drittick@gmail.com>
2017-02-15 14:57:17 -0600

    File Data is fetched. Client and server constructors.

--- src/Peer.java
+++ src/Peer.java
@@ -1,7 +1,7 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,8 +11,8 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Peer {
     // After reading the config file, these fields will be initialized.
     private static String IP;
-    private static String PORT;
-    private static Integer NUMBER_OF_SERVERS;
+    private static Integer PORT;
+    private static Integer NUMBER_OF_PEERS;
 
     //Stream to read user input.
     private static BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
@@ -26,6 +26,9 @@ public class Peer {
     //Peer ID and sequence number of Message ID
     private static String peerID;
     private static Integer sequenceNumber = 0;
+
+    // List of files of this peer.
+    private static ArrayList<String> myFiles = new ArrayList<>();
 
     public static void main(String[] args) {
         //--------------Getting Peer ID-----------------------------------------
@@ -41,35 +44,96 @@ public class Peer {
 
         for(Integer key : configFileData.keySet()){
             if (key == Integer.parseInt(peerID)) {
+                // GET NEIGHBORS
                 String[] split = configFileData.get(key).split(":");
                 String[] neighbors = split[2].split("-");
                 for (int i = 0; i< neighbors.length; i++){
                     peerNeighbors.add(Integer.parseInt(neighbors[i]));
                 }
+
+                //GET IP AND PORT
+                IP = split[0];
+                PORT = Integer.parseInt(split[1]);
             }
         }
 
-        /*for (int j = 0; j < peerNeighbors.size(); j++){
-            System.out.println(peerNeighbors.get(j));
-        }*/
+        NUMBER_OF_PEERS = configFileData.keySet().size();
 
+        //-------Preparing list of files belonging to this peer--------------
+        myFiles = getFileData("Node"+peerID+"/Myfiles");
+
+        //----------Starting Peer Server---------------------------
+        try{
+            ServerSocket peerServer = new ServerSocket(PORT);
+            System.out.println("Peer Server is listening...");
+
+            //-------Starting Peer Client--------------------------
+            new Client(peerID, IP, PORT, NUMBER_OF_PEERS, peerNeighbors).start();
+            System.out.println("Peer Client is running...");
+
+            while (true){
+                Socket newConnection = peerServer.accept();
+                new Server(peerID, IP, PORT, NUMBER_OF_PEERS, peerNeighbors, newConnection).start();
+            }
+        }  catch (IOException e){
+            System.out.println("IOException in main method.");
+        }
     }
 
-    private static class Client extends Thread{
+    private static ArrayList<String> getFileData(String path) {
+        ArrayList<String> fileNames = new ArrayList<>();
 
-
-
-        public void run(){
-
+        File file = new File(path);
+        String[] files = file.list();
+        for (int i = 0; i<files.length; i++){
+            fileNames.add(files[i]);
         }
+
+        return fileNames;
     }
 
     private static class Server extends Thread{
+        private String ID_SERVER;
+        private String IP_SERVER;
+        private Integer PORT_SERVER;
+        private Integer numberOfPeers_SERVER;
+        private ArrayList<Integer> myNeighbors_SERVER;
+        private Socket connection;
 
+        public Server(String peerID, String ip, Integer port, Integer numberOfPeers, ArrayList<Integer> peerNeighbors, Socket newConnection) {
+            ID_SERVER = peerID;
+            IP_SERVER = ip;
+            PORT_SERVER = port;
+            numberOfPeers_SERVER = numberOfPeers;
+            myNeighbors_SERVER = peerNeighbors;
+            connection = newConnection;
+        }
 
         public void run(){
-
+            System.out.println("In run() of server.");
         }
     }
+
+    private static class Client extends Thread{
+        private String ID_CLIENT;
+        private String IP_CLIENT;
+        private Integer PORT_CLIENT;
+        private Integer numberOfPeers_CLIENT;
+        private ArrayList<Integer> myNeighbors_CLIENT;
+
+        public Client(String peerID, String ip, Integer port, Integer numberOfPeers, ArrayList<Integer> peerNeighbors) {
+            ID_CLIENT = peerID;
+            IP_CLIENT = ip;
+            PORT_CLIENT = port;
+            numberOfPeers_CLIENT = numberOfPeers;
+            myNeighbors_CLIENT = peerNeighbors;
+        }
+
+        public void run(){
+            System.out.println("In run() of client.");
+        }
+    }
+
+
 
 }
