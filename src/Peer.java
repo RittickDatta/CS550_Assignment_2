96e7e7acbb5d55d2a81cc43295caa18967de19fd
RittickDatta <drittick@gmail.com>
2017-02-15 18:58:44 -0600

    CHM of Sockets removed. Query Object Created.

--- src/Peer.java
+++ src/Peer.java
@@ -3,6 +3,7 @@ import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -22,6 +23,9 @@ public class Peer {
 
     //Peer ID to Neighbors List
     private static ArrayList<Integer> peerNeighbors = new ArrayList<>();
+
+    //Peer ID to IP & PORT
+    private static ConcurrentHashMap<Integer, String> peerIdtoIPAndPort = new ConcurrentHashMap<>();
 
     //Peer ID and sequence number of Message ID
     private static String peerID;
@@ -59,6 +63,12 @@ public class Peer {
 
         NUMBER_OF_PEERS = configFileData.keySet().size();
 
+        //---------Peer ID to IP & Port---------
+        for(Integer key : configFileData.keySet()){
+            String[] split = configFileData.get(key).split(":");
+            peerIdtoIPAndPort.put(key, split[0]+":"+split[1]);
+        }
+
         //-------Preparing list of files belonging to this peer--------------
         myFiles = getFileData("Node"+peerID+"/Myfiles");
 
@@ -68,12 +78,12 @@ public class Peer {
             System.out.println("Peer Server is listening...");
 
             //-------Starting Peer Client--------------------------
-            new Client(peerID, IP, PORT, NUMBER_OF_PEERS, peerNeighbors).start();
+            new Client(peerID, IP, PORT, NUMBER_OF_PEERS, peerNeighbors, peerIdtoIPAndPort).start();
             System.out.println("Peer Client is running...");
 
             while (true){
                 Socket newConnection = peerServer.accept();
-                new Server(peerID, IP, PORT, NUMBER_OF_PEERS, peerNeighbors, newConnection).start();
+                new Server(peerID, IP, PORT, NUMBER_OF_PEERS, peerNeighbors, newConnection, peerIdtoIPAndPort).start();
             }
         }  catch (IOException e){
             System.out.println("IOException in main method.");
@@ -112,14 +122,16 @@ public class Peer {
         private Integer numberOfPeers_SERVER;
         private ArrayList<Integer> myNeighbors_SERVER;
         private Socket connection;
+        private ConcurrentHashMap<Integer, String> peerIdToIPAndPort_SERVER;
 
-        public Server(String peerID, String ip, Integer port, Integer numberOfPeers, ArrayList<Integer> peerNeighbors, Socket newConnection) {
+        public Server(String peerID, String ip, Integer port, Integer numberOfPeers, ArrayList<Integer> peerNeighbors, Socket newConnection, ConcurrentHashMap<Integer, String> peerIdtoIPAndPort) {
             ID_SERVER = peerID;
             IP_SERVER = ip;
             PORT_SERVER = port;
             numberOfPeers_SERVER = numberOfPeers;
             myNeighbors_SERVER = peerNeighbors;
             connection = newConnection;
+            peerIdToIPAndPort_SERVER = peerIdtoIPAndPort;
         }
 
         public void run(){
@@ -128,22 +140,51 @@ public class Peer {
     }
 
     private static class Client extends Thread{
+        //----------------------Basic Information for Client
         private String ID_CLIENT;
         private String IP_CLIENT;
         private Integer PORT_CLIENT;
         private Integer numberOfPeers_CLIENT;
         private ArrayList<Integer> myNeighbors_CLIENT;
+        private ConcurrentHashMap<Integer, Socket> peerIdToSocket = new ConcurrentHashMap<>();
+        private ConcurrentHashMap<Integer, String> peerIdToIPAndPort_CLIENT;
 
-        public Client(String peerID, String ip, Integer port, Integer numberOfPeers, ArrayList<Integer> peerNeighbors) {
+        //----------------------Variables to Communicate
+        Socket[] sockets = null;//new Socket[myNeighbors_CLIENT.size()];
+        BufferedReader keyboardInput = null;
+        ObjectInputStream inputStream = null;
+        ObjectOutputStream outputStream = null;
+        Query query = null;
+        QueryHit queryHit = null;
+
+        //----------Variable for filename to search
+        private String fileName = null;
+
+        public Client(String peerID, String ip, Integer port, Integer numberOfPeers, ArrayList<Integer> peerNeighbors, ConcurrentHashMap<Integer, String> peerIdtoIPAndPort) {
             ID_CLIENT = peerID;
             IP_CLIENT = ip;
             PORT_CLIENT = port;
             numberOfPeers_CLIENT = numberOfPeers;
             myNeighbors_CLIENT = peerNeighbors;
+            peerIdToIPAndPort_CLIENT = peerIdtoIPAndPort;
         }
 
         public void run(){
+            sockets = new Socket[myNeighbors_CLIENT.size()];
+            keyboardInput = new BufferedReader(new InputStreamReader(System.in));
+
+
             System.out.println("In run() of client.");
+            System.out.println("Enter Name of File to Search:");
+            try {
+                fileName = keyboardInput.readLine();
+            } catch (IOException e) {
+                System.out.println("IOException while user entering filename to search.");
+            }
+
+            Query query = new Query(new MessageID(peerID, ++sequenceNumber), 3, fileName);
+            System.out.println(query.getMessageID().getSequenceNumber());
+
         }
     }
 
